ac3891d04f182887d0db960fefa400fda4c65264
garima310 <garimaa298@gmail.com>
2026-03-28 17:44:49 +0530

    2.11-2.15 completed

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
   return (
@@ -15,24 +16,35 @@ const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNum
     </form>
   )
 }
-const Persons = ({ persons }) => {
+
+const Persons = ({ persons, deletePerson }) => {
   return (
     <ul>
       {persons.map(person => (
-        <li key={person.name}>
+        <li key={person.id}>
           {person.name}: {person.number}
+          <button onClick={() => deletePerson(person.id, person.name)}>
+            delete
+          </button>
         </li>
       ))}
     </ul>
   )
 }
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
 
+const App = () => {
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
-const [newNumber, setNewNumber] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -41,45 +53,78 @@ const [newNumber, setNewNumber] = useState('')
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
-  const [filter, setFilter] = useState('')
+
+  const deletePerson = (id, name) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      axios
+        .delete(`http://localhost:3001/persons/${id}`)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+        })
+    }
+  }
 
   const addPerson = (event) => {
   event.preventDefault()
 
-  const exists = persons.some(person => person.name === newName)
+  const existingPerson = persons.find(person => person.name === newName)
 
-  if (exists) {
-    alert(`${newName} is already added to phonebook`)
+  if (existingPerson) {
+    const confirmReplace = window.confirm(
+      `${newName} is already added. Replace the old number with a new one?`
+    )
+
+    if (confirmReplace) {
+      const updatedPerson = { ...existingPerson, number: newNumber }
+
+      axios
+        .put(`http://localhost:3001/persons/${existingPerson.id}`, updatedPerson)
+        .then(response => {
+          setPersons(
+            persons.map(p => p.id !== existingPerson.id ? p : response.data)
+          )
+        })
+    }
+
     return
   }
 
-  const newPerson = { name: newName, number: newNumber }
-  setPersons(persons.concat(newPerson))
-  setNewName('')
-  setNewNumber('')
-}
+    const newPerson = { name: newName, number: newNumber }
+
+    axios
+      .post('http://localhost:3001/persons', newPerson)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+      })
+
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-  filter shown with <input value={filter} onChange={(e) => setFilter(e.target.value)} />
-</div>
 
-    <PersonForm 
-  addPerson={addPerson}
-  newName={newName}
-  handleNameChange={handleNameChange}
-  newNumber={newNumber}
-  handleNumberChange={handleNumberChange}
-/>
+      <div>
+        filter shown with{' '}
+        <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </div>
+
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
 
       <h2>Numbers</h2>
-     <Persons persons={persons
-  .filter(person => 
-    person.name.toLowerCase().includes(filter.toLowerCase())
-  )
-} />
+
+      <Persons persons={filteredPersons} deletePerson={deletePerson} />
     </div>
   )
 }
